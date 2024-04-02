32942e2bd9cfdfe1356b8e8bde9e641804921e36
SergeiVasilyev <88876877+SergeiVasilyev@users.noreply.github.com>
2024-04-02 17:53:43 +0300

    update course app in part1

--- part1/course/src/App.jsx
+++ part1/course/src/App.jsx
@@ -1,17 +1,32 @@
 import { useState } from 'react'
 
 const App = () => {
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
 
-  const [ counter, setCounter ] = useState(0)
+  const [allClicks, setAll] = useState([])
 
 
-  setTimeout(
-    () => setCounter(counter + 1),
-    1000
-  )
-  console.log('rendering...', counter)
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
+  }
+
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+  }
+
   return (
-    <div>{counter}</div>
+    <div>
+      {left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
+      {right}
+
+      <p>{allClicks.join(' ')}</p>
+    </div>
   )
 }
 
