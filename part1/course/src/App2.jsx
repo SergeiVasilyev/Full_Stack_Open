70e7de1791c4a5adf0464ed66adb0525e3bfb5ef
SergeiVasilyev <88876877+SergeiVasilyev@users.noreply.github.com>
2024-04-03 11:25:21 +0300

    button component and handler added

--- part1/course/src/App2.jsx
+++ part1/course/src/App2.jsx
@@ -1,6 +1,12 @@
 import { useState } from 'react'
 
 
+const Button = (props) => (
+    <button onClick={props.handleClick}>
+      {props.text}
+    </button>
+  )
+
 const App = () => {
     const [value, setValue] = useState(10)
   
@@ -12,11 +18,19 @@ const App = () => {
       return handler
     }
 
+    // Return a function inside a function
     const setToValue = (newValue) => () => {
         console.log('value now', newValue)  // print the new value to console
         setValue(newValue)
     }
+
+    // Return a function
+    const setToValue2 = newValue => {
+        console.log('value now', newValue)
+        setValue(newValue)
+    }
   
+    
     return (
       <div>
         {value}
@@ -24,10 +38,14 @@ const App = () => {
         <button onClick={hello('world')}>button</button> 
         <button onClick={hello('react')}>button</button>
         <button onClick={hello('function')}>button</button>
-
+        <br />
         <button onClick={setToValue(1000)}>thousand</button>
         <button onClick={setToValue(0)}>reset</button>
         <button onClick={setToValue(value + 1)}>increment</button>
+        <br />
+        <Button handleClick={() => setToValue2(1000)} text="thousand" />
+        <Button handleClick={() => setToValue2(0)} text="reset" />
+        <Button handleClick={() => setToValue2(value + 1)} text="increment" />
       </div>
     )
   }
