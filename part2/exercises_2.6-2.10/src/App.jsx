aa78cbb361977f70eb85eea5c6cc967c43a7637e
SergeiVasilyev <88876877+SergeiVasilyev@users.noreply.github.com>
2024-04-17 10:14:31 +0300

    exercise 2.7 added

--- part2/exercises_2.6-2.10/src/App.jsx
+++ part2/exercises_2.6-2.10/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import './App.css'
 
 const App = () => {
   const [persons, setPersons] = useState([
@@ -6,19 +7,43 @@ const App = () => {
   ]) 
   const [newName, setNewName] = useState('')
 
+  const handleNoteChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const addNote = (event) => {
+    event.preventDefault()
+    console.log(persons, newName)
+
+    if (persons.find(person => person.name === newName)) {
+      return alert(`${newName} is already added to phonebook`)
+    }
+
+    const noteObject = {
+      name: newName
+    }
+    setPersons(persons.concat(noteObject))
+    setNewName('')
+    
+  }
+
   return (
-    <div>
+    <div className='app'>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addNote}>
         <div>
-          name: <input />
-        </div>
-        <div>
+          <input value={newName} onChange={handleNoteChange} />
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <div>
+        <ul className='no-bullets'>
+        {persons.map(person =>
+          <li key={person.name}>{person.name}</li>
+        )}
+        </ul>
+      </div>
     </div>
   )
 }
