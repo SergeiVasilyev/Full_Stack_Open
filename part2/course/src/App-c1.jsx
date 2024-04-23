e5c488c5cc691cc4a23f50758dfd8be7e81a92d0
SergeiVasilyev <88876877+SergeiVasilyev@users.noreply.github.com>
2024-04-23 12:49:23 +0300

    Part2 Course. Getting data from server

--- part2/course/src/App-c1.jsx
+++ part2/course/src/App-c1.jsx
@@ -1,25 +1,11 @@
-import { useState, useEffect } from 'react'
-import axios from 'axios'
+import { useState } from 'react'
 import Note from './components/Note'
 
 
 const App = (props) => {
-  const [notes, setNotes] = useState([])
+  const [notes, setNotes] = useState(props.notes)
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
-
-
-  const hook = () => {
-    console.log('effect')
-    axios
-      .get('http://localhost:3001/notes')
-      .then(response => {
-        console.log('promise fulfilled')
-        setNotes(response.data)
-      })
-  }
-  useEffect(hook, [])
-
 
   const notesToShow = showAll
     ? notes
