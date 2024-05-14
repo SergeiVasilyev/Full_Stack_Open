3411e2dc5fd059568357b6216d4be534b0080cd3
SergeiVasilyev <88876877+SergeiVasilyev@users.noreply.github.com>
2024-05-14 11:25:27 +0300

    express.static added

--- part3/course/backend/index.js
+++ part3/course/backend/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+app.use(express.static('dist'))
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
@@ -36,9 +37,9 @@ let notes = [
   }
 ]
 
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
-})
+// app.get('/', (request, response) => {
+//   response.send('<h1>Hello World!</h1>')
+// })
 
 app.get('/api/notes', (request, response) => {
   response.json(notes)
