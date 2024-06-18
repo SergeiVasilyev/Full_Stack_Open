13108a41011cdab952c6ac3792f857708afe72fc
SergeiVasilyev <88876877+SergeiVasilyev@users.noreply.github.com>
2024-06-18 10:40:36 +0300

    ValidationError (Validation in mongoose schema)

--- part3/course/backend/index.js
+++ part3/course/backend/index.js
@@ -77,21 +77,23 @@ const generateId = () => {
   return maxId + 1
 }
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
   const body = request.body
 
-  if (body.content === undefined) {
-    return response.status(400).json({ error: 'content missing' })
-  }
+  // if (body.content === undefined) {
+  //   return response.status(400).json({ error: 'content missing' })
+  // }
 
   const note = new Note({
     content: body.content,
     important: body.important || false,
   })
 
-  note.save().then(savedNote => {
-    response.json(savedNote)
-  })
+  note.save()
+    .then(savedNote => {
+      response.json(savedNote)
+    })
+    .catch(error => next(error))
 })
 
 
@@ -103,7 +105,10 @@ app.put('/api/notes/:id', (request, response, next) => {
     important: body.important,
   }
 
-  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+  Note.findByIdAndUpdate(request.params.id, note, { new: true, runValidators: true, context: 'query' })
+  // new: true - return the updated note
+  // runValidators: true - validate the input data
+  // context: 'query' - use the query middleware
     .then(updatedNote => {
       response.json(updatedNote)
     })
@@ -122,7 +127,9 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
-  } 
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
 
   next(error)
 }
