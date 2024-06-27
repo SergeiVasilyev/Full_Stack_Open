5ad44120bdc25a92a0facf5c9e78f1f16e75a25c
SergeiVasilyev <88876877+SergeiVasilyev@users.noreply.github.com>
2024-06-27 11:56:58 +0300

    4.4: Helper Functions and Unit Tests, step 2

--- part4/exercises/backend/utils/list_helper.js
+++ part4/exercises/backend/utils/list_helper.js
@@ -2,6 +2,14 @@ const dummy = (blogs) => {
   return 1
 }
 
-module.exports = {
-  dummy
+const totalLikes = (blogs) => {
+  const sum = (sum, item) => sum + item.likes
+  return blogs.reduce(sum, 0)
 }
+
+module.exports = {
+  dummy,
+  totalLikes
+}
+
+
