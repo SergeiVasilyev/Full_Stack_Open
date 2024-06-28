bd42a7901a1fdc5db12b0485f12d87359429a884
SergeiVasilyev <88876877+SergeiVasilyev@users.noreply.github.com>
2024-06-28 10:14:42 +0300

    4.5*: Helper Functions and Unit Tests, step 3

--- part4/exercises/backend/utils/list_helper.js
+++ part4/exercises/backend/utils/list_helper.js
@@ -7,9 +7,24 @@ const totalLikes = (blogs) => {
   return blogs.reduce(sum, 0)
 }
 
+
+const favoriteBlog = (blogs) => { 
+  if (blogs.length === 0) {
+    return null
+  }
+  const likesArr = blogs.map(blog => blog.likes)
+  const maxLikes = Math.max(...likesArr)
+  const blog = blogs.find(blog => blog.likes === maxLikes)
+  delete blog._id
+  delete blog.__v
+  delete blog.url
+  return blog
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
 
 
