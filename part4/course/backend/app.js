9c85306f098a2f58733696aa622d377f1c3d44cc
SergeiVasilyev <88876877+SergeiVasilyev@users.noreply.github.com>
2024-07-25 12:07:53 +0300

    Token authentication

--- part4/course/backend/app.js
+++ part4/course/backend/app.js
@@ -9,6 +9,7 @@ const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 
 mongoose.set('strictQuery', false)
@@ -28,6 +29,7 @@ app.use(express.static('dist'))
 app.use(express.json())
 app.use(middleware.requestLogger)
 
+app.use('/api/login', loginRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/notes', notesRouter)
 
