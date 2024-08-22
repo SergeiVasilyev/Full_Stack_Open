009e6b1543cb5ce678a4eae442ab8895090533e8
SergeiVasilyev <88876877+SergeiVasilyev@users.noreply.github.com>
2024-08-22 10:54:59 +0300

    Course 5: PropTypes and ESLint

--- part5/course/frontend/src/components/LoginForm.jsx
+++ part5/course/frontend/src/components/LoginForm.jsx
@@ -1,13 +1,21 @@
-const LoginForm = (props) => {
+import PropTypes from 'prop-types'
+
+const LoginForm = ({
+	handleSubmit,
+	handleUsernameChange,
+	handlePasswordChange,
+	username,
+	password
+   }) => {
 	return (
 	  <div>
 			<h2>Login</h2>
-			<form onSubmit={props.handleSubmit}>
+			<form onSubmit={handleSubmit}>
 				<div>
 					username
 					<input
-						value={props.username}
-						onChange={props.handleUsernameChange}
+						value={username}
+						onChange={handleUsernameChange}
 						name="username"
 					/>
 				</div>
@@ -15,14 +23,22 @@ const LoginForm = (props) => {
 					password
 					<input
 						type="password"
-						value={props.password}
-						onChange={props.handlePasswordChange}
+						value={password}
+						onChange={handlePasswordChange}
 					/>
 				</div>
 				<button type="submit">login</button>
 			</form>
 	  </div>
 	)
+  }
+
+  LoginForm.propTypes = {
+	handleSubmit: PropTypes.func.isRequired,
+	handleUsernameChange: PropTypes.func.isRequired,
+	handlePasswordChange: PropTypes.func.isRequired,
+	username: PropTypes.string.isRequired,
+	password: PropTypes.string.isRequired
   }
 
   export default LoginForm
