46bf8abb6e79edf697979d90baab96159733a99b
SergeiVasilyev <88876877+SergeiVasilyev@users.noreply.github.com>
2024-09-03 09:43:07 +0300

    Course 5. Clicking buttons in tests

--- part5/course/frontend/src/components/Note.test.jsx
+++ part5/course/frontend/src/components/Note.test.jsx
@@ -1,7 +1,8 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Note from './Note'
 
-test('renders content', () => {
+test('renders content', async () => {
   const note = {
     content: 'Component testing is done with react-testing-library',
     important: true
@@ -15,10 +16,23 @@ test('renders content', () => {
   // expect(element).toBeDefined()
 
   // Rendering a component 2
-  const { container } = render(<Note note={note} />)
-  screen.debug() // Show code of component
-  const div = container.querySelector('.note')
-  expect(div).toHaveTextContent(
-    'Component testing is done with react-testing-library'
+  // const { container } = render(<Note note={note} />)
+  // screen.debug() // Show code of component
+  // const div = container.querySelector('.note')
+  // expect(div).toHaveTextContent(
+  //   'Component testing is done with react-testing-library'
+  // )
+
+  const mockHandler = vi.fn()
+  render(
+    <Note note={note} toggleImportance={mockHandler} />
   )
+
+  const user = userEvent.setup()
+  const button = screen.getByText('Make not important')
+  await user.click(button)
+
+  expect(mockHandler.mock.calls).toHaveLength(1)
+
+
 })
