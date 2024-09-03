f0443738118c211e423973ccca0efb41c5d84ae8
SergeiVasilyev <88876877+SergeiVasilyev@users.noreply.github.com>
2024-09-03 09:34:50 +0300

    Debugging tests. print the HTML of a component to the terminal

--- part5/course/frontend/src/components/Note.test.jsx
+++ part5/course/frontend/src/components/Note.test.jsx
@@ -7,8 +7,18 @@ test('renders content', () => {
     important: true
   }
 
-  render(<Note note={note} />)
+  // Rendering a component 1
+  // render(<Note note={note} />)
+  // screen.debug()
+  // const element = screen.getByText('Component testing is done with react-testing-library')
+  // screen.debug(element) // Show element in console
+  // expect(element).toBeDefined()
 
-  const element = screen.getByText('Component testing is done with react-testing-library')
-  expect(element).toBeDefined()
+  // Rendering a component 2
+  const { container } = render(<Note note={note} />)
+  screen.debug() // Show code of component
+  const div = container.querySelector('.note')
+  expect(div).toHaveTextContent(
+    'Component testing is done with react-testing-library'
+  )
 })
