ff61d5e64c6e81bea43367f637c00c5539e46f97
SergeiVasilyev <88876877+SergeiVasilyev@users.noreply.github.com>
2024-09-30 19:12:12 +0300

    Exercise 5.20. the blog can be liked

--- part5/exercises/e2e_tests/tests/blog_app.spec.js
+++ part5/exercises/e2e_tests/tests/blog_app.spec.js
@@ -34,4 +34,51 @@ describe('Blog app', () => {
       await expect(page.getByText('Superuser logged-in')).not.toBeVisible()
     })
   })
+
+  describe('When logged in', () => {
+    beforeEach(async ({ page }) => {
+      await page.locator('input[name="Username"]').fill('user1');
+      await page.locator('input[name="Password"]').fill('salainen');
+      await page.getByRole('button', { name: 'login' }).click();
+    })
+  
+    test('a new blog can be created', async ({ page }) => {
+      await page.getByRole('button', { name: 'Create new blog' }).click()
+      await page.locator('input[name="Title"]').fill('Test blog')
+      await page.locator('input[name="Author"]').fill('Test author')
+      await page.locator('input[name="Url"]').fill('https://test.com')
+      await page.getByRole('button', { name: 'Create' }).click()
+
+      await expect(page.getByText('a new blog Test blog by Test author added')).toBeVisible()
+      await expect(page.getByText('Test author see more')).toBeVisible()
+
+      // await page.getByText('see more').click()
+      // await expect(page.getByText('https://test.com')).toBeVisible()
+    })
+
+    test('blog can be liked', async ({ page }) => {
+      await page.getByRole('button', { name: 'Create new blog' }).click()
+      await page.locator('input[name="Title"]').fill('Test blog')
+      await page.locator('input[name="Author"]').fill('Test author')
+      await page.locator('input[name="Url"]').fill('https://test.com')
+      await page.getByRole('button', { name: 'Create' }).click()
+
+      await page.getByRole('button', { name: 'Create new blog' }).click()
+      await page.locator('input[name="Title"]').fill('Test blog2')
+      await page.locator('input[name="Author"]').fill('Test author2')
+      await page.locator('input[name="Url"]').fill('https://test.com')
+      await page.getByRole('button', { name: 'Create' }).click()
+
+      await page.getByText('a new blog Test blog2 by Test author2 added').click()
+
+      await page.getByText('Test author2 see more').getByRole('button').click()
+      await expect(page.getByText('Test author2').locator('..').nth(1).getByText('likes 0')).toBeVisible()
+      await expect(page.getByText('Test author2').locator('..').getByRole('button', { name: 'like' })).toBeVisible()
+
+      await page.getByText('Test author2').locator('..').getByRole('button', { name: 'like' }).click()
+      await expect(page.getByText('Test author2').locator('..').getByText('likes 1')).toBeVisible()
+    })
+
+
+  })
 })
