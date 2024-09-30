8514b61f2c87de567ea877d8fee519dca41711c5
SergeiVasilyev <88876877+SergeiVasilyev@users.noreply.github.com>
2024-09-30 11:21:30 +0300

    Exercise 5.18. Login

--- part5/exercises/e2e_tests/tests/blog_app.spec.js
+++ part5/exercises/e2e_tests/tests/blog_app.spec.js
@@ -1,8 +1,16 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
 
 describe('Blog app', () => {
-  beforeEach(async ({ page }) => {
-    await page.goto('http://localhost:5174')
+  beforeEach(async ({ page, request }) => {
+    await request.post('/api/testing/reset')
+    await request.post('/api/users', {
+      data: {
+        name: 'Superuser',
+        username: 'user1',
+        password: 'salainen'
+      }
+    })
+    await page.goto('/')
   })
 
   test('Login form is shown', async ({ page }) => {
@@ -11,17 +19,19 @@ describe('Blog app', () => {
     await expect(page.locator('button[type="submit"]')).toBeVisible()
   })
 
-  // describe('Login', () => {
-  //   test('succeeds with correct credentials', async ({ page }) => {
-  //     // ...
-        // await page.locator('input[name="Username"]').fill('User1');
-        // await page.locator('input[name="Password"]').fill('salainen');
-        // await page.getByRole('button', { name: 'login' }).click();
-        // await expect(page.getByText('Superuser logged in')).toBeVisible()
-  //   })
+  describe('Login', () => {
+    test('succeeds with correct credentials', async ({ page }) => {
+      await page.locator('input[name="Username"]').fill('user1');
+      await page.locator('input[name="Password"]').fill('salainen');
+      await page.getByRole('button', { name: 'login' }).click();
+      await expect(page.getByText('Superuser logged-in')).toBeVisible()
+    })
 
-  //   test('fails with wrong credentials', async ({ page }) => {
-  //     // ...
-  //   })
-  // })
+    test('fails with wrong credentials', async ({ page }) => {
+      await page.locator('input[name="Username"]').fill('user2');
+      await page.locator('input[name="Password"]').fill('salainen');
+      await page.getByRole('button', { name: 'login' }).click();
+      await expect(page.getByText('Superuser logged-in')).not.toBeVisible()
+    })
+  })
 })
